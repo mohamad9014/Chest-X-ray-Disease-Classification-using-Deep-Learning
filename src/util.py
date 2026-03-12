a1070e471a34cd108c78a50c13b22c9d7ceb0fd9
mohamad naseri <121359931+mohamad9014@users.noreply.github.com>
2026-03-12 17:19:56 +0100

    Add files via upload

--- src/util.py
+++ src/util.py
@@ -1,120 +1,108 @@
-import random
-
-import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras import backend as K
-from tensorflow.keras.preprocessing import image
-from sklearn.metrics import roc_auc_score, roc_curve
-from tensorflow.compat.v1.logging import INFO, set_verbosity
-import os
-
-random.seed(a=None, version=2)
-
-set_verbosity(INFO)
+import pandas as pd
+from sklearn.metrics import (
+    average_precision_score,
+    precision_recall_curve,
+    roc_auc_score,
+    roc_curve,
+)
 
 
-def get_mean_std_per_batch(image_dir, df, H=320, W=320):
-    sample_data = []
-    for img in df.sample(100)["Image"].values:
-        image_path = os.path.join(image_dir, img)
-        sample_data.append(
-            np.array(image.load_img(image_path, target_size=(H, W))))
-
-    mean = np.mean(sample_data, axis=(0, 1, 2, 3))
-    std = np.std(sample_data, axis=(0, 1, 2, 3), ddof=1)
-    return mean, std
+def get_true_pos(y, pred, th=0.5):
+    pred_t = (pred > th)
+    return np.sum((pred_t == True) & (y == 1))
 
 
-def load_image(img, image_dir, df, preprocess=True, H=320, W=320):
-    """Load and preprocess image."""
-    mean, std = get_mean_std_per_batch(image_dir, df, H=H, W=W)
-    img_path = os.path.join(image_dir, img)
-    x = image.load_img(img_path, target_size=(H, W))
-    if preprocess:
-        x -= mean
-        x /= std
-        x = np.expand_dims(x, axis=0)
-    return x
+def get_true_neg(y, pred, th=0.5):
+    pred_t = (pred > th)
+    return np.sum((pred_t == False) & (y == 0))
 
 
-def grad_cam(input_model, image, cls, layer_name, H=320, W=320):
-    import tensorflow as tf
-
-    grad_model = tf.keras.models.Model(
-        [input_model.inputs],
-        [input_model.get_layer(layer_name).output, input_model.output]
-    )
-
-    with tf.GradientTape() as tape:
-        conv_outputs, predictions = grad_model(image)
-        loss = predictions[:, cls]
-
-    grads = tape.gradient(loss, conv_outputs)
-
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    conv_outputs = conv_outputs[0]
-    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap)
-
-    heatmap = np.maximum(heatmap, 0)
-
-    if np.max(heatmap) != 0:
-        heatmap /= np.max(heatmap)
-
-    heatmap = cv2.resize(heatmap, (W, H), cv2.INTER_LINEAR)
-
-    return heatmap
+def get_false_neg(y, pred, th=0.5):
+    pred_t = (pred > th)
+    return np.sum((pred_t == False) & (y == 1))
 
 
-def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
-                    layer_name='bn'):
-    preprocessed_input = load_image(img, image_dir, df)
-    predictions = model.predict(preprocessed_input)
-
-    print("Loading original image")
-    plt.figure(figsize=(15, 10))
-    plt.subplot(151)
-    plt.title("Original")
-    plt.axis('off')
-    plt.imshow(load_image(img, image_dir, df, preprocess=False), cmap='gray')
-
-    j = 1
-    for i in range(len(labels)):
-        if labels[i] in selected_labels:
-            print(f"Generating gradcam for class {labels[i]}")
-            gradcam = grad_cam(model, preprocessed_input, i, layer_name)
-            plt.subplot(151 + j)
-            plt.title(f"{labels[i]}: p={predictions[0][i]:.3f}")
-            plt.axis('off')
-            plt.imshow(load_image(img, image_dir, df, preprocess=False),
-                       cmap='gray')
-            plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
-            j += 1
+def get_false_pos(y, pred, th=0.5):
+    pred_t = (pred > th)
+    return np.sum((pred_t == True) & (y == 0))
 
 
-def get_roc_curve(labels, predicted_vals, generator):
-    auc_roc_vals = []
-    for i in range(len(labels)):
-        try:
-            gt = generator.labels[:, i]
-            pred = predicted_vals[:, i]
-            auc_roc = roc_auc_score(gt, pred)
-            auc_roc_vals.append(auc_roc)
-            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
-            plt.figure(1, figsize=(10, 10))
+def get_performance_metrics(y, pred, class_labels, tp=get_true_pos,
+                            tn=get_true_neg, fp=get_false_pos,
+                            fn=get_false_neg,
+                            acc=None, prevalence=None, spec=None,
+                            sens=None, ppv=None, npv=None, auc=None, f1=None,
+                            thresholds=[]):
+    if len(thresholds) != len(class_labels):
+        thresholds = [0.5] * len(class_labels)
+
+    columns = ["Label", "TP", "TN", "FP", "FN", "Accuracy", "Prevalence",
+               "Sensitivity", "Specificity", "PPV", "NPV", "AUC", "F1", "Threshold"]
+
+    rows = []
+
+    for i in range(len(class_labels)):
+        row = [
+            class_labels[i],
+            round(tp(y[:, i], pred[:, i], thresholds[i]), 3) if tp is not None else "Not Defined",
+            round(tn(y[:, i], pred[:, i], thresholds[i]), 3) if tn is not None else "Not Defined",
+            round(fp(y[:, i], pred[:, i], thresholds[i]), 3) if fp is not None else "Not Defined",
+            round(fn(y[:, i], pred[:, i], thresholds[i]), 3) if fn is not None else "Not Defined",
+            round(acc(y[:, i], pred[:, i], thresholds[i]), 3) if acc is not None else "Not Defined",
+            round(prevalence(y[:, i]), 3) if prevalence is not None else "Not Defined",
+            round(sens(y[:, i], pred[:, i], thresholds[i]), 3) if sens is not None else "Not Defined",
+            round(spec(y[:, i], pred[:, i], thresholds[i]), 3) if spec is not None else "Not Defined",
+            round(ppv(y[:, i], pred[:, i], thresholds[i]), 3) if ppv is not None else "Not Defined",
+            round(npv(y[:, i], pred[:, i], thresholds[i]), 3) if npv is not None else "Not Defined",
+            round(auc(y[:, i], pred[:, i]), 3) if auc is not None else "Not Defined",
+            round(f1(y[:, i], pred[:, i] > thresholds[i]), 3) if f1 is not None else "Not Defined",
+            round(thresholds[i], 3)
+        ]
+        rows.append(row)
+
+    df = pd.DataFrame(rows, columns=columns)
+    df = df.set_index("Label")
+    return df
+
+
+def print_confidence_intervals(class_labels, statistics):
+    df = pd.DataFrame(columns=["Mean AUC (CI 5%-95%)"])
+    for i in range(len(class_labels)):
+        mean = statistics.mean(axis=1)[i]
+        max_ = np.quantile(statistics, .95, axis=1)[i]
+        min_ = np.quantile(statistics, .05, axis=1)[i]
+        df.loc[class_labels[i]] = ["%.2f (%.2f-%.2f)" % (mean, min_, max_)]
+    return df
+
+
+def get_curve(gt, pred, target_names, curve='roc'):
+    for i in range(len(target_names)):
+        if curve == 'roc':
+            curve_function = roc_curve
+            auc_roc = roc_auc_score(gt[:, i], pred[:, i])
+            label = target_names[i] + " AUC: %.3f " % auc_roc
+            xlabel = "False positive rate"
+            ylabel = "True positive rate"
+            a, b, _ = curve_function(gt[:, i], pred[:, i])
+            plt.figure(1, figsize=(7, 7))
             plt.plot([0, 1], [0, 1], 'k--')
-            plt.plot(fpr_rf, tpr_rf,
-                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
-            plt.xlabel('False positive rate')
-            plt.ylabel('True positive rate')
-            plt.title('ROC curve')
-            plt.legend(loc='best')
-        except:
-            print(
-                f"Error in generating ROC curve for {labels[i]}. "
-                f"Dataset lacks enough examples."
-            )
-    plt.show()
-    return auc_roc_vals
+            plt.plot(a, b, label=label)
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+
+            plt.legend(loc='upper center', bbox_to_anchor=(1.3, 1),
+                       fancybox=True, ncol=1)
+        elif curve == 'prc':
+            precision, recall, _ = precision_recall_curve(gt[:, i], pred[:, i])
+            average_precision = average_precision_score(gt[:, i], pred[:, i])
+            label = target_names[i] + " Avg.: %.3f " % average_precision
+            plt.figure(1, figsize=(7, 7))
+            plt.step(recall, precision, where='post', label=label)
+            plt.xlabel('Recall')
+            plt.ylabel('Precision')
+            plt.ylim([0.0, 1.05])
+            plt.xlim([0.0, 1.0])
+            plt.legend(loc='upper center', bbox_to_anchor=(1.3, 1),
+                       fancybox=True, ncol=1)
